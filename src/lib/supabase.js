82bc953493542162494ca194275ae50f7e2b2658
pocapoc2001 <alexandruthegreat2001@gmail.com>
2025-12-17 19:46:01 +0200

    v3

--- src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Înlocuiește string-urile de mai jos cu cele din proiectul lui Sebastian
-const supabaseUrl = 'https://xffruzexdawupvqwewek.supabase.co'; 
-const supabaseKey = 'sb_publishable_CEd81dGEI5F-0TdFb10j7A_3Ny2SGA-'; 
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
